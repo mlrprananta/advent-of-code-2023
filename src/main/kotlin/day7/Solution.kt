a245aa600f8fd188d7cbbd58e5c9ef777541e523
mlrprananta <lukeprananta@gmail.com>
2023-12-18 08:19:37 +0100

    Add day 10 part 1 solution

---
+++ src/main/kotlin/day7/Solution.kt
@@ -0,0 +1,11 @@
+package day7
+
+import util.BaseSolution
+
+class Solution(resourcePath: String) : BaseSolution(resourcePath) {
+    private val regex = """(?<cards>\w+) (?<bet>\d+)""".toRegex()
+    fun solve(resourcePath: String): Int {
+        return 0
+    }
+
+}
